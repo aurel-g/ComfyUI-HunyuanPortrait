2a96b9e6863b96373d943f8fc295c450bd6ff4f8
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-28 17:08:12 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
+from .nodes import LoadHunyuanPortraitImage, LoadHunyuanPortraitVideo, LoadHunyuanPortraitConfig, HunyuanPortrait
 
+NODE_CLASS_MAPPINGS = {
+    "LoadHunyuanPortraitImage": LoadHunyuanPortraitImage,
+    "LoadHunyuanPortraitVideo": LoadHunyuanPortraitVideo,
+    "LoadHunyuanPortraitConfig": LoadHunyuanPortraitConfig,
+    "HunyuanPortrait": HunyuanPortrait,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadHunyuanPortraitImage": "Load HunyuanPortrait Image",
+    "LoadHunyuanPortraitVideo": "Load HunyuanPortrait Video",
+    "LoadHunyuanPortraitConfig": "Load HunyuanPortrait Config",
+    "HunyuanPortrait": "HunyuanPortrait",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
